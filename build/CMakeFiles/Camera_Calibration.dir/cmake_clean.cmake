478ed0fe031e8a05f5194eac669d73cd2014220d
xudaojin <xudaojin007@outlook.com>
2020-02-20 21:47:57 +0800

    first commit

---
+++ build/CMakeFiles/Camera_Calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Camera_Calibration.dir/camera_calibration.o"
+  "Camera_Calibration.pdb"
+  "Camera_Calibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Camera_Calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
